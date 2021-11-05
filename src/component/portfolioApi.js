672d526d06e77497db4ae9356e10a0b1515c109b
Rajeev Kumar <92941997+Rajeev1503@users.noreply.github.com>
2021-11-05 20:50:30 +0530

    Update portfolioApi.js

--- src/component/portfolioApi.js
+++ src/component/portfolioApi.js
@@ -2,14 +2,14 @@
 const PortfolioApi = [
 
             {   id:1,
-                image:"rkrajeev/images/one.png",
+                image:"images/one.png",
                 name:"Text Utility",
                 description:"This web app is created to help users modify/edit their text and use it after copying. This site is built on react js library using core javascript for functions and bootstrap for styling"
                 
             },
 
             {   id:2,
-                image:"rkrajeev/images/two.png",
+                image:"images/two.png",
                 name:"Portfolio Site",
                 description:"This is a portfolio website created using react js and bootstrap"   
             }
