a541ab8f4ec55b2b9619449fcdfa26e87b48f0b8
Rajeev1503 <rk.rajeevkr1503@gmail.com>
2021-11-05 20:38:16 +0530

    trial4

--- src/component/portfolioApi.js
+++ src/component/portfolioApi.js
@@ -2,14 +2,14 @@
 const PortfolioApi = [
 
             {   id:1,
-                image:"images/one.png",
+                image:"https://github.com/Rajeev1503/rkrajeev/blob/gh-pages/images/one.png",
                 name:"Text Utility",
                 description:"This web app is created to help users modify/edit their text and use it after copying. This site is built on react js library using core javascript for functions and bootstrap for styling"
                 
             },
-            
+
             {   id:2,
-                image:"images/two.png",
+                image:"https://github.com/Rajeev1503/rkrajeev/blob/gh-pages/images/two.png",
                 name:"Portfolio Site",
                 description:"This is a portfolio website created using react js and bootstrap"   
             }
